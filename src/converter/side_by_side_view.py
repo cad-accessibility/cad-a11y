ce6b3f06cce234300720d46768d27d23ef9c1400
Felix Hahnlein <fhahnlei@Felixs-MacBook-Pro-1364.local>
2026-06-26 13:47:36 -0700

    chore: clean-up pass. Remove unused files and functions #34

--- src/converter/side_by_side_view.py
+++ src/converter/side_by_side_view.py
@@ -8,39 +8,13 @@ import os, json
 import matplotlib.pyplot as plt
 import trimesh
 from .render_low_res import get_outlines
-from .plane_intersection_utils import depth_peeling_single_depth_with_bbox, faces_on_plane, compute_area, faces_on_plane_fast
+from .plane_intersection_utils import depth_peeling_single_depth_with_bbox, faces_on_plane_fast
 from .single_view_stl import get_single_view
 
 from OCC.Core.STEPControl import STEPControl_Reader
 from OCC.Extend.DataExchange import write_stl_file
 from OCC.Core.gp import gp_Pnt, gp_Dir 
 
-#views = {
-#    "top": {
-#        "eye": gp_Pnt(0, 0, -1000),
-#        "dir": gp_Dir(0, 0, 1)
-#    },
-#    "front": {
-#        "eye": gp_Pnt(0, -1000, 0),
-#        "dir": gp_Dir(0, 1, 0)
-#    },
-#    "left": {
-#        "eye": gp_Pnt(-1000, 0, 0),
-#        "dir": gp_Dir(1, 0, 0)
-#    },
-#    "bottom": {
-#        "eye": gp_Pnt(0, 0, 1000),
-#        "dir": gp_Dir(0, 0, -1)
-#    },
-#    "back": {
-#        "eye": gp_Pnt(0, 1000, 0),
-#        "dir": gp_Dir(0, -1, 0)
-#    },
-#    "right": {
-#        "eye": gp_Pnt(1000, 0, 0),
-#        "dir": gp_Dir(-1, 0, 0)
-#    }
-#}
 views = {
     "top": {
         "eye": np.array([0, 0, -1000.0]),
@@ -71,7 +45,6 @@ views = {
 def get_side_view(shape, bbox, cut_depth=0.9, view_key_legend="top", view_key_cut="left",  rendering_mode="filled", imposed_ax_limits_legend=[], imposed_ax_limits_cut=[], screen_size=[60, 40]):
 
     print("get_side_view: rendering mode", rendering_mode, "view_key_legend", view_key_legend, "view_key_cut", view_key_cut)
-    #shape_brep_copy = deepcopy(shape_brep)
     # Calculate dimensions based on screen_size
     total_width = screen_size[0]
     legend_width = int(total_width / 3)
@@ -79,12 +52,8 @@ def get_side_view(shape, bbox, cut_depth=0.9, view_key_legend="top", view_key_cu
     total_height = screen_size[1]
     
     # Cut view
-    #cut_depth = 0.5
     normal_dir = views[view_key_cut]["dir"]
-    #shape_brep_cut, plane_origin = depth_peeling_single_depth_with_bbox(shape_brep, gp_Dir(normal_dir.X(), normal_dir.Y(), normal_dir.Z()), 
-    #                                                              depth=cut_depth, bbox=bbox)
     shape_cut, plane_origin = depth_peeling_single_depth_with_bbox(shape, normal_dir, depth=cut_depth, bbox=bbox)
-    #shape_cut = faces_on_plane(shape_cut, plane_origin, normal_dir)
     shape_cut = faces_on_plane_fast(shape_cut, plane_origin, normal_dir)
 
     # Target pixel resolution (cut view gets 2/3 of width)
@@ -94,11 +63,8 @@ def get_side_view(shape, bbox, cut_depth=0.9, view_key_legend="top", view_key_cu
     ax = fig.add_axes([0, 0, 1, 1])  # Fill entire figure
     ax.axis('off')
 
-    #area = compute_area(shape_brep_cut)
     #if not np.isclose(area, 0.0):
     if type(shape_cut) != list and len(shape_cut.faces) > 0 and not np.isclose(shape_cut.area, 0.0):
-        #write_stl_file(shape_brep_cut, "model.stl", linear_deflection=0.1)
-        #shape = trimesh.load_mesh("model.stl")
 
         colors = [0.0 for i in range(len(shape_cut.faces))]
         if view_key_cut == "top":
@@ -134,16 +100,7 @@ def get_side_view(shape, bbox, cut_depth=0.9, view_key_legend="top", view_key_cu
 
     img = Image.open(buf)
     img_np = np.array(img)
-    #for i in range(img_np.shape[0]):
-    #    for j in range(img_np.shape[1]):
-    #        if img_np[i,j,0] == 255:
-    #            print(1, end='')
-    #        else:
-    #            print(0, end='')
-    #    print()
 
-    #plt.imshow(img_np)
-    #plt.show()
     if plt.fignum_exists(fig.number):
         plt.close(fig.number)
 
@@ -154,8 +111,6 @@ def get_side_view(shape, bbox, cut_depth=0.9, view_key_legend="top", view_key_cu
         cut_img = img_np
 
     # Legend - view
-    #shape_brep = shape_brep_copy
-
     normal_dir = views[view_key_legend]["dir"]
 
     # Target pixel resolution (legend view gets 1/3 of width)
@@ -165,10 +120,7 @@ def get_side_view(shape, bbox, cut_depth=0.9, view_key_legend="top", view_key_cu
     ax = fig.add_axes([0, 0, 1, 1])  # Fill entire figure
     ax.axis('off')
 
-    #area = compute_area(shape_brep)
     if not np.isclose(shape.area, 0.0):
-        #write_stl_file(shape_brep, "model.stl", linear_deflection=0.1)
-        #shape = trimesh.load_mesh("model.stl")
 
         colors = [0.0 for i in range(len(shape.faces))]
         if view_key_legend == "top":
@@ -212,16 +164,7 @@ def get_side_view(shape, bbox, cut_depth=0.9, view_key_legend="top", view_key_cu
 
     img = Image.open(buf)
     img_np = np.array(img)
-    #for i in range(img_np.shape[0]):
-    #    for j in range(img_np.shape[1]):
-    #        if img_np[i,j,0] == 255:
-    #            print(1, end='')
-    #        else:
-    #            print(0, end='')
-    #    print()
 
-    #plt.imshow(img_np)
-    #plt.show()
     if plt.fignum_exists(fig.number):
         plt.close(fig.number)
     # Left panel (legend view) stays visually stable across render mode changes.
@@ -231,17 +174,13 @@ def get_side_view(shape, bbox, cut_depth=0.9, view_key_legend="top", view_key_cu
     width_px, height_px = legend_width, total_height
     dpi = 100 
     fig = plt.figure(figsize=(width_px / dpi, height_px / dpi), dpi=dpi)
-    ax = fig.add_axes([0, 0, 1, 1])  # Fill entire figure
+    ax = fig.add_axes([0, 0, 1, 1])  
     ax.axis('off')
     # Add line at plane_origin in legend_img
     line_vec = np.cross(views[view_key_cut]["dir"], views[view_key_legend]["dir"])
 
     plane_origin_np = np.array(plane_origin)
-    #print("plane_origin_np", plane_origin_np)
-    #print("line_vec", line_vec)
     line_pts = np.array([plane_origin_np - 1000.0*line_vec, plane_origin_np + 1000.0*line_vec])
-    #print("view_limits_legend", imposed_ax_limits_legend)
-    #print("line pts", line_pts)
     if view_key_legend == "top":
         coords = line_pts[:,[0,1]]
     if view_key_legend == "front":
@@ -259,7 +198,6 @@ def get_side_view(shape, bbox, cut_depth=0.9, view_key_legend="top", view_key_cu
         coords = line_pts[:,[1,2]]
         coords[:,0] *= -1
     ax.plot(coords[:, 0], coords[:, 1], linewidth=0.5, color=(0.0,0.0,0.0,1.0), aa=False)
-    #print("line coords", coords)
     ax.set_aspect('equal')
     ax = plt.gca()
     ax.set_xlim(legend_ax_limits[0])
@@ -278,29 +216,13 @@ def get_side_view(shape, bbox, cut_depth=0.9, view_key_legend="top", view_key_cu
         for j in range(img_np.shape[1]):
             if img_np[i,j,0] == 0:
                 legend_img[i,j,0] = 0
-            #    print(1, end='')
-            #else:
-            #    print(0, end='')
-        #print()
-    
-    #line_img = img_np
 
-    #plt.imshow(img_np)
-    #plt.show()
     if plt.fignum_exists(fig.number):
         plt.close(fig.number)
 
     combined_view = np.zeros([total_height, total_width, 4], dtype=legend_img.dtype) 
     combined_view[:, :legend_width] = legend_img
     combined_view[:, legend_width:] = cut_img
-    #for i in range(combined_view.shape[0]):
-    #    for j in range(combined_view.shape[1]):
-    #        if combined_view[i,j,0] == 255:
-    #            print(1, end='')
-    #        else:
-    #            print(combined_view[i, j])
-    #            print(0, end='')
-    #    print()
 
     return combined_view, -1
 
